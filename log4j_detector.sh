c14d60d270d4f61fa3908b1cc8d390e54a000e16
RoiSec <roi>
2021-12-15 11:25:09 +0200

    edit code

--- log4j_detector.sh
+++ log4j_detector.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-check_variables () {
-if [ -z ${LOG4J_FORMAT_MSG_NO_LOOKUPS} ]; then
-  echo "Not Enviroment Variable Found"
-else
-  echo "LOG4J_FORMAT_MSG_NO_LOOKUPS enviroment variable found!"
-fi
 
-proc=$(ps -ef | grep java | grep -v grep)
-result=$(ps -ef | grep java | grep -v grep | grep log4j2.formatMsgNoLookups=true | wc -l)
-# echo "Found java procces: " $proc
-if [ $((result)) -eq 0 ]; then
-        echo "Not Found java System property!"
-else
-        echo "Found java System property!"
-fi
+check_variables () {
+    if [ -z ${LOG4J_FORMAT_MSG_NO_LOOKUPS} ]; then
+    echo "Not Enviroment Variable Found!"
+    else
+    echo "LOG4J_FORMAT_MSG_NO_LOOKUPS enviroment variable found!"
+    fi
+
+    proc=$(ps -ef | grep java | grep -v grep)
+    result=$(ps -ef | grep java | grep -v grep | grep log4j2.formatMsgNoLookups=true | wc -l)
+    # echo "Found java procces: " $proc
+    if [ $((result)) -eq 0 ]; then
+            echo "Not Found java System property!"
+    else
+            echo "Found java System property!"
+    fi
 }
 check_container () {
     for containerId in $(docker ps -q)
@@ -26,3 +27,7 @@ check_variables
 if  docker info > /dev/null 2>&1; then
     check_container
 fi
+check_jar(){
+    
+
+}
