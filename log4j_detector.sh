f4a1cacc3fc44c2b6d2ba3a5b0aa6cd41f0426a1
RoiSec <roi>
2021-12-15 15:12:58 +0200

    edit code

--- log4j_detector.sh
+++ log4j_detector.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit
+fi
 check_variables () {
     if [ -z ${LOG4J_FORMAT_MSG_NO_LOOKUPS} ]; then
     echo "Not Enviroment Variable Found!"
@@ -20,13 +24,13 @@ check_container () {
     for containerId in $(docker ps -q)
     do
         echo "Image Name:" ;docker ps  -f "id=$containerId" --format '{{.Image}}'
-        docker exec $containerId sh -c 'wget -qO - https://raw.githubusercontent.com/RoiSec/log4j_detector/main/log4j_detector.sh $@ | sh'
+        docker exec $containerId sh -c 'wget -qO - https://raw.githubusercontent.com/RoiSec/log4j_detector/main/log4j_detector.sh | sh'
     done
 }
 # check_variables
-# if  docker info > /dev/null 2>&1; then
-#     check_container
-# fi
+if  docker info > /dev/null 2>&1; then
+    check_container
+fi
 check_jar(){
     echo "Checking jars"
     jars_paths=("$@")
