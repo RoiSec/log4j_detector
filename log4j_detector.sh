8b4fc3ffa10951d1b71c548c62d22a92c0069891
RoiSec <roi>
2021-12-16 17:00:17 +0200

    ggffdgfdgf

--- log4j_detector.sh
+++ log4j_detector.sh
@@ -32,7 +32,7 @@ check_jar(){
     FILE=$1
     java_path=$(find /usr/ /bin/ -executable -name java -type f 2>/dev/null | head -n 1)
     # echo $java_path
-    eval $java_path -jar logpresso-log4j2-scan-1.6.3.jar $FILE >>/tmp/out.txt 2>/dev/null
+    eval $java_path -jar /tmp/logpresso-log4j2-scan-1.6.3.jar $FILE >>/tmp/out.txt 
     grep -i 'Found CVE-2021-44228' /tmp/out.txt 2>/dev/null
     rm /tmp/out.txt 2>/dev/null
         # else
