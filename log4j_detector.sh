976d0630dbfaa61aecbae1b5873ddda61a1e4432
RoiSec <roi>
2021-12-16 17:10:21 +0200

    aaaaa

--- log4j_detector.sh
+++ log4j_detector.sh
@@ -22,7 +22,7 @@ check_variables () {
     fi
 }
 check_jar(){
-    echo "Checking jars"
+    # echo "Checking jars"
     if ! command -v curl &> /dev/null
     then
         wget 'https://raw.githubusercontent.com/RoiSec/log4j_detector/main/logpresso/logpresso-log4j2-scan-1.6.3.jar' --no-check-certificate -q -O '/tmp/logpresso-log4j2-scan-1.6.3.jar'
@@ -30,7 +30,7 @@ check_jar(){
         curl -s 'https://raw.githubusercontent.com/RoiSec/log4j_detector/main/logpresso/logpresso-log4j2-scan-1.6.3.jar' -o '/tmp/logpresso-log4j2-scan-1.6.3.jar' 2>/dev/null
     fi
     FILE=$1
-    java_path=$(find /usr/ /bin/ -executable -name java -type f 2>/dev/null | head -n 1)
+    java_path=$(find /usr/ /bin/ -executable -name java -type f -perm /a+x 2>/dev/null | head -n 1)
     # echo $java_path
     eval $java_path -jar /tmp/logpresso-log4j2-scan-1.6.3.jar $FILE >>/tmp/out.txt 
     grep -i 'Found CVE-2021-44228' /tmp/out.txt 2>/dev/null
